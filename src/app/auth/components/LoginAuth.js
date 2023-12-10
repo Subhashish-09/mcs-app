ead577a52ef84f6455d2f92a29842b7491aeca73
Subhashish-05 <subhashish2005@outlook.in>
2023-12-10 14:58:23 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/auth/components/LoginAuth.js 	modified:   src/app/auth/components/RegisterAuth.js 	deleted:    src/components/ui/form.jsx 	deleted:    src/components/ui/label.jsx 	modified:   tailwind.config.js

--- src/app/auth/components/LoginAuth.js
+++ src/app/auth/components/LoginAuth.js
@@ -1,92 +1,52 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { signInWithEmailAndPassword } from "../actions";
 import { useRouter } from "next/navigation";
 import { Button, Input } from "@nextui-org/react";
-
-const FormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1, {
-    message: "Password is required.",
-  }),
-});
+import { useFormik } from "formik";
 
 const LoginAuth = ({ next }) => {
   const router = useRouter();
 
-  const form = useForm({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
+  const formik = useFormik({
+    initialValues: {
       email: "",
       password: "",
     },
+    onSubmit: async (values) => {
+      const result = await signInWithEmailAndPassword(
+        values.email,
+        values.password
+      );
+      const { error } = JSON.parse(result);
+      if (!error) {
+        router.replace(next ?? "/");
+      }
+    },
   });
 
-  async function onSubmit(data) {
-    const result = await signInWithEmailAndPassword(data.email, data.password);
-    const { error } = JSON.parse(result);
-    if (!error) {
-      router.replace(next ?? "/");
-    }
-  }
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  variant="bordered"
-                  placeholder="example@gmail.com"
-                  {...field}
-                  type="email"
-                  onChange={field.onChange}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  variant="bordered"
-                  placeholder="password"
-                  {...field}
-                  type="password"
-                  onChange={field.onChange}
-                />
-              </FormControl>
+    <form onSubmit={formik.handleSubmit} className="w-full space-y-6">
+      <Input
+        name="email"
+        variant="bordered"
+        placeholder="example@gmail.com"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button color="primary" type="submit" className="w-full flex gap-2">
-          SignIn
-        </Button>
-      </form>
-    </Form>
+      <Input
+        variant="bordered"
+        placeholder="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+
+      <Button color="primary" type="submit" className="w-full flex gap-2">
+        SignIn
+      </Button>
+    </form>
   );
 };
 
--- src/app/auth/components/RegisterAuth.js
+++ src/app/auth/components/RegisterAuth.js
@@ -1,121 +1,61 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-
 import { signUpWithEmailAndPassword } from "../actions";
 import { useRouter } from "next/navigation";
 import { Input, Button } from "@nextui-org/react";
-
-const FormSchema = z
-  .object({
-    email: z.string().email(),
-    password: z.string().min(6, {
-      message: "Password is required.",
-    }),
-    confirm: z.string().min(6, {
-      message: "Password is required.",
-    }),
-  })
-  .refine((data) => data.confirm === data.password, {
-    message: "Password did not match",
-    path: ["confirm"],
-  });
+import { useFormik } from "formik";
 
 const RegisterAuth = ({ next }) => {
   const router = useRouter();
-  const form = useForm({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
+
+  const formik = useFormik({
+    initialValues: {
       email: "",
       password: "",
-      confirm: "",
+      confirm_password: "",
+    },
+    onSubmit: async (values) => {
+      const result = await signUpWithEmailAndPassword(
+        values.email,
+        values.password
+      );
+      const { error } = JSON.parse(result);
+      if (!error) {
+        router.reload();
+      }
     },
   });
 
-  async function onSubmit(data) {
-    const result = await signUpWithEmailAndPassword(data.email, data.password);
-    const { error } = JSON.parse(result);
-    if (!error) {
-      router.reload();
-    }
-  }
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  variant="bordered"
-                  placeholder="example@gmail.com"
-                  {...field}
-                  type="email"
-                  onChange={field.onChange}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  variant="bordered"
-                  placeholder="password"
-                  {...field}
-                  type="password"
-                  onChange={field.onChange}
-                />
-              </FormControl>
+    <form onSubmit={formik.handleSubmit} className="w-full space-y-6">
+      <Input
+        name="email"
+        variant="bordered"
+        placeholder="example@gmail.com"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirm"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input
-                  variant="bordered"
-                  placeholder="Confirm Password"
-                  {...field}
-                  type="password"
-                  onChange={field.onChange}
-                />
-              </FormControl>
+      <Input
+        name="password"
+        variant="bordered"
+        placeholder="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button color="primary" type="submit" className="w-full flex gap-2">
-          Register
-        </Button>
-      </form>
-    </Form>
+      <Input
+        name="confirm_password"
+        variant="bordered"
+        placeholder="Confirm Password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.confirm_password}
+      />
+      <Button color="primary" type="submit" className="w-full flex gap-2">
+        Register
+      </Button>
+    </form>
   );
 };
 
